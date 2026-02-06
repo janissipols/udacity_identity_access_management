8efc23f95a07a6780c1695c955e5d542690d3f67
j_sipols <j_sipols>
2026-02-06 14:31:14 +0000

    POST, PATCH drinks, GET drink details

--- Project/03_coffee_shop_full_stack/starter_code/backend/src/api.py
+++ Project/03_coffee_shop_full_stack/starter_code/backend/src/api.py
@@ -30,39 +30,67 @@ def get_drinks():
         'drinks': [drink.short() for drink in drinks]
     })
 
-'''
-@TODO implement endpoint
-    GET /drinks-detail
-        it should require the 'get:drinks-detail' permission
-        it should contain the drink.long() data representation
-    returns status code 200 and json {"success": True, "drinks": drinks} where drinks is the list of drinks
-        or appropriate status code indicating reason for failure
-'''
+
+@app.route('/drinks-detail')
+# @requires_auth('get:drinks-detail')
+def get_drinks_detail(payload):
+    drinks = Drink.query.order_by(Drink.id).all()
+
+    return jsonify({
+        'success': True,
+        'drinks': [drink.long() for drink in drinks]
+    })
 
 
-'''
-@TODO implement endpoint
-    POST /drinks
-        it should create a new row in the drinks table
-        it should require the 'post:drinks' permission
-        it should contain the drink.long() data representation
-    returns status code 200 and json {"success": True, "drinks": drink} where drink an array containing only the newly created drink
-        or appropriate status code indicating reason for failure
-'''
+@app.route('/drinks', methods=['POST'])
+#@requires_auth('post:drinks')
+def create_drink(payload):
+    body = request.get_json()
+    if body is None:
+        abort(400)
+    title = body.get('title', None)
+    recipe = body.get('recipe', None)
+    if title is None or recipe is None:
+        abort(422)
+    try:
+        drink = Drink(title=title, recipe=json.dumps(recipe))
+        drink.insert()
+    except Exception:
+        abort(422)
+
+    return jsonify({
+        'success': True,
+        'drinks': [drink.long()]
+    })
 
 
-'''
-@TODO implement endpoint
-    PATCH /drinks/<id>
-        where <id> is the existing model id
-        it should respond with a 404 error if <id> is not found
-        it should update the corresponding row for <id>
-        it should require the 'patch:drinks' permission
-        it should contain the drink.long() data representation
-    returns status code 200 and json {"success": True, "drinks": drink} where drink an array containing only the updated drink
-        or appropriate status code indicating reason for failure
-'''
+@app.route('/drinks/<int:id>', methods=['PATCH'])
+#@requires_auth('patch:drinks')
+def update_drink(payload, id):
+    body = request.get_json()
+    if body is None:
+        abort(400)
 
+    drink = Drink.query.filter(Drink.id == id).one_or_none()
+    if drink is None:
+        abort(404)
+
+    title = body.get('title', None)
+    recipe = body.get('recipe', None)
+
+    if title is not None:
+        drink.title = title
+    if recipe is not None:
+        drink.recipe = json.dumps(recipe)
+    try:
+        drink.update()
+    except Exception:
+        abort(422)
+
+    return jsonify({
+        'success': True,
+        'drinks': [drink.long()]
+    })
 
 '''
 @TODO implement endpoint
